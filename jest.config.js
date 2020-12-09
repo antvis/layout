7a8d059d7558cbb9dd7256eacf085bfeffc4c699
shiwu <shiwu.wyy@antgroup.com>
2020-12-09 15:30:08 +0800

    test: g6 grid test

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,12 @@ module.exports = {
   preset: 'ts-jest',
   collectCoverage: false,
   collectCoverageFrom: ['src/**/*.{ts,js}', '!**/node_modules/**', '!**/vendor/**'],
+  testRegex: '/tests/.*-spec\\.ts?$',
   moduleDirectories: ['node_modules', 'src'],
   moduleFileExtensions: ['js', 'ts', 'json'],
   moduleNameMapper: {
-    '@g6/types': '<rootDir>/types',
-    '@g6/(.*)': '<rootDir>/src/$1',
+    '@layout/types': '<rootDir>/types',
+    '@layout/(.*)': '<rootDir>/src/$1',
   },
   globals: {
     'ts-jest': {
