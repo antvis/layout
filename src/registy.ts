d6f670da0ad3d2f36d04749716189ee614622ee6
shiwu <shiwu.wyy@antgroup.com>
2020-12-09 14:54:51 +0800

    feat: add fruchterman layout and concentric layout

---
+++ src/registy.ts
@@ -0,0 +1,21 @@
+const map: Map<string, any> = new Map()
+
+export const registLayout = (name: string, customClass: any) => {
+  if (map.get(name)) {
+    throw new Error('already have a layout with the same name')
+  }
+  map.set(name, customClass)
+}
+
+export const unRegistLayout = (name: string) => {
+  if (map.has(name)) {
+    map.delete(name)
+  }
+}
+
+export const getLayoutByName = (name: string) => {
+  if (map.has(name)) {
+    return map.get(name)
+  }
+  return null
+}
