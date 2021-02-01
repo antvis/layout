b487a1206578c6224013d747017550a7919f8d76
聚则 <zhanning.bzn@antgroup.com>
2021-02-01 12:05:32 +0800

    fix: refactor layout packages

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,44 @@
-export * from './layout'
-export * from './registy'
+import {
+  Layout,
+  Layouts,
+  ILayout,
+  GridLayout,
+  RandomLayout,
+  GForceLayout,
+  ForceLayout,
+  CircularLayout,
+  DagreLayout,
+  RadialLayout,
+  ConcentricLayout,
+  MDSLayout,
+  FruchtermanLayout,
+  FruchtermanGPULayout,
+  GForceGPULayout,
+  ComboForceLayout
+} from "./layout";
+import { registerLayout, unRegisterLayout, getLayoutByName } from "./registy";
+
+// Layout class & types
+export { Layout, Layouts, ILayout };
+
+// register layout
+export { registerLayout, unRegisterLayout, getLayoutByName };
+
+export {
+  GridLayout,
+  RandomLayout,
+  GForceLayout,
+  ForceLayout,
+  CircularLayout,
+  DagreLayout,
+  RadialLayout,
+  ConcentricLayout,
+  MDSLayout,
+  FruchtermanLayout,
+  FruchtermanGPULayout,
+  GForceGPULayout,
+  ComboForceLayout
+};
+
+// layout worker
+export * from "./layout/worker";
