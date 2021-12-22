2d873eb697d34ae682de22d82c44e14d71b51aa8
Yanyan-Wang <yanyanwang93@gmail.com>
2021-12-22 10:37:17 +0800

    fix: array flat compatibility;

--- src/layout/dagre/src/graphlib.ts
+++ src/layout/dagre/src/graphlib.ts
@@ -8,4 +8,16 @@ if (!graphlib && typeof window !== "undefined") {
   graphlib = (window as any).graphlib;
 }
 
+Object.defineProperty(Array.prototype, 'flat', {
+  value: function (depth = 1) {
+    return this.reduce((flat: Array<any>, toFlatten: Array<any>) => {
+      return flat.concat(
+        Array.isArray(toFlatten) && depth > 1
+          ? toFlatten.flat(depth - 1)
+          : toFlatten
+      );
+    }, []);
+  },
+});
+
 export default graphlib;
