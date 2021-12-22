00e79379691f255a165fcff26b28d7dd13f147a9
Yanyan-Wang <yanyanwang93@gmail.com>
2021-12-22 17:44:17 +0800

    fix: flat and reduce problem

--- src/layout/dagre/src/graphlib.ts
+++ src/layout/dagre/src/graphlib.ts
@@ -7,17 +7,40 @@ let graphlib = glib;
 if (!graphlib && typeof window !== "undefined") {
   graphlib = (window as any).graphlib;
 }
-
-Object.defineProperty(Array.prototype, 'flat', {
-  value (depth = 1) {
-    return this.reduce((flat: any, toFlatten: any) => {
-      return flat.concat(
-        Array.isArray(toFlatten) && depth > 1
-          ? toFlatten.flat(depth - 1)
-          : toFlatten
-      );
-    }, []);
-  },
-});
+// @ts-ignore
+(Array as any).prototype.flat = function(count) {
+  let c = count || 1;
+  let len = this.length;
+  let ret: any = [];
+  if (this.length == 0) return this;
+  while (c--) {
+    let _arr = [];
+    let flag = false;
+    if (ret.length == 0) {
+      flag = true;
+      for (let i = 0; i < len; i++) {
+        if (this[i] instanceof Array) {
+          ret.push(...this[i]);
+        } else {
+          ret.push(this[i]);
+        }
+      }
+    } else {
+      for (let i = 0; i < ret.length; i++) {
+        if (ret[i] instanceof Array) {
+          flag = true;
+          _arr.push(...ret[i]);
+        } else {
+          _arr.push(ret[i]);
+        }
+      }
+      ret = _arr;
+    }
+    if (!flag && c == Infinity) {
+      break;
+    }
+  }
+  return ret;
+};
 
 export default graphlib;
