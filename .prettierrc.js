0f0f3a0c91107037e8ae4b88d6ba86745129ad86
simplejason <simplejason.coder@gmail.com>
2022-03-04 11:54:18 +0800

    chore: add eslint prettier and husky

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  useTabs: false,
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  proseWrap: 'preserve',
+  trailingComma: 'none',
+  endOfLine: 'lf'
+};
