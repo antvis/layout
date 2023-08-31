52cbff2acd39e5d7f47523e9f5de46ca75122b01
pomelo.lcw <pomelo.lcw@antgroup.com>
2023-08-31 21:26:42 +0800

    chore: update docs

--- packages/layout/src/types.ts
+++ packages/layout/src/types.ts
@@ -1,11 +1,11 @@
 import {
-  Graph as IGraph,
-  Node as INode,
-  Edge as IEdge,
-  PlainObject,
-  GraphView as IGraphView,
   ID,
-} from '@antv/graphlib';
+  Edge as IEdge,
+  Graph as IGraph,
+  GraphView as IGraphView,
+  Node as INode,
+  PlainObject,
+} from "@antv/graphlib";
 
 export interface NodeData extends PlainObject {
   size?: number | number[];
@@ -130,61 +130,141 @@ export interface LayoutSupervisor {
   isRunning(): boolean;
 }
 
+/**
+ * Interface for configuring a circular layout.
+ */
 export interface CircularLayoutOptions {
+  /** The center coordinates of the circle. */
   center?: PointTuple;
+  /** The width of the layout. */
   width?: number;
+  /** The height of the layout. */
   height?: number;
+  /** The radius of the circle. */
   radius?: number | null;
+  /** The starting radius. */
   startRadius?: number | null;
+  /** The ending radius. */
   endRadius?: number | null;
+  /** Whether to arrange in a clockwise direction. */
   clockwise?: boolean;
+  /** The number of divisions. */
   divisions?: number;
-  ordering?: 'topology' | 'topology-directed' | 'degree' | null;
+  /** The node ordering method, which can be 'topology', 'topology-directed', or 'degree'. */
+  ordering?: "topology" | "topology-directed" | "degree" | null;
+  /** The angle ratio. */
   angleRatio?: number;
+  /** The starting angle. */
   startAngle?: number;
+  /** The ending angle. */
   endAngle?: number;
+  /** The spacing between nodes, which can be a function or a number. */
   nodeSpacing?: ((node?: Node) => number) | number;
+  /** The size of the nodes, which can be a number, an array of numbers, or a function. */
   nodeSize?: number | number[] | ((nodeData: Node) => number);
 }
 
+/**
+ * Interface for grid layout options
+ */
 export interface GridLayoutOptions {
+  /**
+   * Optional, width of the grid
+   */
   width?: number;
+  /**
+   * Optional, height of the grid
+   */
   height?: number;
+  /**
+   * Optional, starting point of the grid
+   */
   begin?: PointTuple;
+  /**
+   * Optional, whether to prevent node overlap
+   */
   preventOverlap?: boolean;
+  /**
+   * Optional, size of the node. Can be a number, an array of numbers or a function
+   */
   nodeSize?: number | number[] | ((nodeData: Node) => number);
+  /**
+   * Optional, padding value when preventing overlap
+   */
   preventOverlapPadding?: number;
+  /**
+   * Optional, whether to condense the grid
+   */
   condense?: boolean;
+  /**
+   * Optional, number of rows in the grid
+   */
   rows?: number;
+  /**
+   * Optional, number of columns in the grid
+   */
   cols?: number;
+  /**
+   * Optional, basis for sorting
+   */
   sortBy?: string;
+  /**
+   * Optional, function for node position
+   */
   position?: (node?: Node) => { row?: number; col?: number };
+  /**
+   * Optional, spacing between nodes. Can be a number or a function
+   */
   nodeSpacing?: ((node?: Node) => number) | number;
 }
 
+/**
+ * RandomLayoutOptions interface
+ * @property center - The center point of the layout
+ * @property width - The width of the layout
+ * @property height - The height of the layout
+ */
 export interface RandomLayoutOptions {
   center?: PointTuple;
   width?: number;
   height?: number;
 }
 
+/**
+ * MDSLayoutOptions interface
+ * @property center - The center point of the layout
+ * @property linkDistance - The distance between links in the layout
+ */
 export interface MDSLayoutOptions {
   center?: PointTuple;
   linkDistance?: number;
 }
 
+/** Export interface: Concentric Layout Options */
 export interface ConcentricLayoutOptions {
+  /** Center point */
   center?: PointTuple;
+  /** Prevent overlap */
   preventOverlap?: boolean;
+  /** Node size */
   nodeSize?: number | PointTuple | ((nodeData: Node) => number);
+  /** Sweep angle */
   sweep?: number;
+  /** Equidistant */
   equidistant?: boolean;
+  /** Start angle */
   startAngle?: number;
+  /** Clockwise or not */
   clockwise?: boolean;
+  /** Maximum level difference */
   maxLevelDiff?: number;
+  /** Sort by */
   sortBy?: string;
+  /** Width */
   width?: number;
+  /** Height */
   height?: number;
+  /** Node spacing */
   nodeSpacing?: number | number[] | ((node?: Node) => number);
 }
 
@@ -206,67 +286,118 @@ export interface RadialLayoutOptions {
 }
 
 export type DagreRankdir =
-  | 'TB'
-  | 'BT'
-  | 'LR'
-  | 'RL'
-  | 'tb'
-  | 'lr'
-  | 'rl'
-  | 'bt';
-export type DagreAlign = 'UL' | 'UR' | 'DL' | 'DR';
-
+  | "TB"
+  | "BT"
+  | "LR"
+  | "RL"
+  | "tb"
+  | "lr"
+  | "rl"
+  | "bt";
+export type DagreAlign = "UL" | "UR" | "DL" | "DR";
+/** Export interface: Dagre Layout Options */
 export interface DagreLayoutOptions {
+  /** Rank direction */
   rankdir?: DagreRankdir;
+  /** Alignment */
   align?: DagreAlign;
+  /** Begin point */
   begin?: PointTuple;
+  /** Node size */
   nodeSize?: number | number[] | ((nodeData: Node) => number);
+  /** Node separation */
   nodesep?: number;
+  /** Rank separation */
   ranksep?: number;
+  /** Control points */
   controlPoints?: boolean;
+  /** Sort by combo */
   sortByCombo?: boolean;
+  /** Edge label space */
   edgeLabelSpace?: boolean;
+  /** Node order */
   nodeOrder?: string[];
-  radial?: boolean; // 是否基于 dagre 进行辐射布局
-  focusNode?: ID | Node | null; // radial 为 true 时生效，关注的节点
+  /** Radial layout based on dagre */
+  radial?: boolean;
+  /** Focus node for radial layout */
+  focusNode?: ID | Node | null;
+  /** Preset nodes and edges */
   preset?: {
     nodes: OutNode[];
     edges: Edge[];
   };
+  /** Function for node separation */
   nodesepFunc?: (d?: Node) => number;
+  /** Function for rank separation */
   ranksepFunc?: (d?: Node) => number;
 }
 
+/** Export interface: D3 Force Layout Options */
 export interface D3ForceLayoutOptions {
+  /** Center point */
   center?: PointTuple;
+  /** Link distance */
   linkDistance?: number | ((edge?: Edge) => number);
+  /** Edge strength */
   edgeStrength?: number | ((edge?: Edge) => number);
+  /** Node strength */
   nodeStrength?: number | ((node?: Node) => number);
+  /** Prevent overlap */
   preventOverlap?: boolean;
+  /** Collide strength */
   collideStrength?: number;
+  /** Node size */
   nodeSize?: number | number[] | ((node?: Node) => number);
+  /** Node spacing */
   nodeSpacing?: number | number[] | ((node?: Node) => number);
+  /** Alpha value */
   alpha?: number;
+  /** Alpha decay rate */
   alphaDecay?: number;
+  /** Minimum alpha value */
   alphaMin?: number;
+  /** Clustering enabled or not */
   clustering?: boolean;
+  /** Cluster node strength */
   clusterNodeStrength?: number;
+  /** Cluster edge strength */
   clusterEdgeStrength?: number;
+  /** Cluster edge distance */
   clusterEdgeDistance?: number;
+  /** Cluster node size */
   clusterNodeSize?: number;
+  /** Cluster foci strength */
   clusterFociStrength?: number;
+  /** Force simulation object */
   forceSimulation?: any;
+  /** On tick callback function*/
   onTick?: (data: LayoutMapping) => void;
 }
 
 export interface CentripetalOptions {
-  /** Force strength for leaf nodes. */
+  /**
+   * Force strength for leaf nodes.
+   * This option specifies the strength of the centripetal force applied to leaf nodes.
+   * It can be a number or a function that takes the current node, all nodes, and all edges as arguments and returns a number.
+   */
   leaf?: number | ((node: Node, nodes: Node[], edges: Edge[]) => number);
-  /** Force strength for single nodes. */
+  /**
+   * Force strength for single nodes.
+   * This option specifies the strength of the centripetal force applied to single nodes (nodes with no edges).
+   * It can be a number or a function that takes the current node as an argument and returns a number.
+   */
   single?: number | ((node: Node) => number);
-  /** Force strength for other nodes. */
+  /**
+   * Force strength for other nodes.
+   * This option specifies the strength of the centripetal force applied to other nodes (nodes that are not leaf or single).
+   * It can be a number or a function that takes the current node as an argument and returns a number.
+   */
   others?: number | ((node: Node) => number);
-  /** Centri force's position and sterngth, points to the canvas center by default */
+  /**
+   * Centri force's position and sterngth, points to the canvas center by default
+   * This option specifies the position and strength of the centripetal force.
+   * It can be a function that takes the current node, all nodes, all edges, canvas width, and canvas height as arguments and returns an object with x, y, z (optional), and centerStrength (optional) properties.
+   */
   center?: (
     node: Node,
     nodes: Node[],
@@ -292,11 +423,16 @@ export interface ComboCombinedLayoutOptions {
 }
 
 interface CommonForceLayoutOptions {
+  /** The number of dimensions for the force layout. */
   dimensions?: number;
+  /** The center point of the force layout. */
   center?: PointTuple;
+  /** The minimum movement required for the force layout to continue iterating. */
   minMovement?: number;
+  /** The maximum number of iterations for the force layout. */
   maxIteration?: number;
-  distanceThresholdMode?: 'mean' | 'max' | 'min';
+  /** The mode for calculating the distance threshold for the force layout. */
+  distanceThresholdMode?: "mean" | "max" | "min";
 
   /**
    * If distance is specified, sets the maximum distance between nodes over which this force is considered.
@@ -305,31 +441,54 @@ interface CommonForceLayoutOptions {
    */
   maxDistance?: number;
 }
-
 export interface ForceLayoutOptions extends CommonForceLayoutOptions {
+  /** The width of the force layout. */
   width?: number;
+  /** The height of the force layout. */
   height?: number;
+  /** The distance between linked nodes in the force layout. */
   linkDistance?: number | ((edge?: Edge, source?: any, target?: any) => number);
+  /** The strength of the force applied to nodes in the force layout. */
   nodeStrength?: number | ((d?: Node) => number);
+  /** The strength of the force applied to edges in the force layout. */
   edgeStrength?: number | ((d?: Edge) => number);
+  /** Whether or not to prevent node overlap in the force layout. */
   preventOverlap?: boolean;
+  /** The size of nodes in the force layout. */
   nodeSize?: number | number[] | ((d?: Node) => number);
+  /** The spacing between nodes in the force layout. */
   nodeSpacing?: number | ((d?: Node) => number);
+  /** The damping factor for the force layout. */
   damping?: number;
+  /** The maximum speed for nodes in the force layout. */
   maxSpeed?: number;
+  /** The scale factor for the Coulomb force in the force layout. */
   coulombDisScale?: number;
+  /** The strength of the gravity force in the force layout. */
   gravity?: number;
+  /** A factor used to adjust various parameters in the force layout. */
   factor?: number;
+  /** The interval between iterations of the force layout. */
   interval?: number;
+  /** Options for centripetal forces in the force layout. */
   centripetalOptions?: CentripetalOptions;
+  /** Whether or not to use leaf clustering in the force layout. */
   leafCluster?: boolean;
+  /** Whether or not to use clustering in the force layout. */
   clustering?: boolean;
+  /** The property used to cluster nodes by in the force layout. */
   nodeClusterBy?: string;
+  /** The strength of the cluster node forces in the force layout. */
   clusterNodeStrength?: number | ((node: Node) => number);
+  /** The strength of collision forces in the force layout. */
   collideStrength?: number;
+  /** A callback function called on each tick of the force layout. */
   onTick?: (data: LayoutMapping) => void;
+  /** A function used to calculate the mass of nodes in the force layout. */
   getMass?: (node?: Node) => number;
+  /** A function used to calculate the center point of nodes in the force layout. */
   getCenter?: (node?: Node, degree?: number) => number[];
+
   monitor?: (params: {
     energy: number;
     nodes: Node[];
@@ -337,30 +496,54 @@ export interface ForceLayoutOptions extends CommonForceLayoutOptions {
     iterations: number;
   }) => void;
 }
-
 export interface ForceAtlas2LayoutOptions extends CommonForceLayoutOptions {
+  /** The width of the force layout. */
   width?: number;
+  /** The height of the force layout. */
   height?: number;
+  /** The repulsion constant for the force layout. */
   kr?: number;
+  /** The gravity constant for the force layout. */
   kg?: number;
+  /** The scaling factor for the force layout. */
   ks?: number;
+  /** The maximum scaling factor for the force layout. */
   ksmax?: number;
+  /** The cooling factor for the force layout. */
   tao?: number;
-  mode?: 'normal' | 'linlog';
+  /** The mode for calculating forces in the force layout. */
+  mode?: "normal" | "linlog";
+  /** Whether or not to prevent node overlap in the force layout. */
   preventOverlap?: boolean;
+  /** Whether or not to dissuade hubs in the force layout. */
   dissuadeHubs?: boolean;
+  /** Whether or not to use the Barnes-Hut algorithm in the force layout. */
   barnesHut?: boolean;
+  /** Whether or not to prune nodes in the force layout. */
   prune?: boolean;
+  /** The size of nodes in the force layout. */
   nodeSize?: number | number[] | ((node?: Node) => number);
+
+  /** A callback function called on each tick of the force layout. */
   onTick?: (data: LayoutMapping) => void;
 }
+
 export interface FruchtermanLayoutOptions extends CommonForceLayoutOptions {
+  /** The width of the force layout. */
   width?: number;
+  /** The height of the force layout. */
   height?: number;
+  /** The strength of the gravity force in the force layout. */
   gravity?: number;
+  /** The speed of the force layout. */
   speed?: number;
+  /** Whether or not to use clustering in the force layout. */
   clustering?: boolean;
+  /** The strength of the cluster gravity force in the force layout. */
   clusterGravity?: number;
+  /** The property used to cluster nodes by in the force layout. */
   nodeClusterBy?: string;
+
+  /** A callback function called on each tick of the force layout. */
   onTick?: (data: LayoutMapping) => void;
 }
