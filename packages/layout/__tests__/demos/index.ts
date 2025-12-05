8051f60c00ab27c419e3702026c30e1b24a09ae3
yvonneyx <zhuyuxin0627@gmail.com>
2025-12-05 15:13:05 +0800

    chore(layout): add MDS layout demo to showcase functionality

--- packages/layout/__tests__/demos/index.ts
+++ packages/layout/__tests__/demos/index.ts
@@ -2,5 +2,6 @@ export { render as circular } from './circular';
 export { render as d3force } from './d3-force';
 export { render as fruchterman } from './fruchterman';
 export { render as grid } from './grid';
+export { render as mds } from './mds';
 export { render as radial } from './radial';
 export { render as random } from './random';
---
+++ packages/layout/__tests__/demos/mds.ts
@@ -0,0 +1,25 @@
+import { MDSLayout } from '@/src';
+import type { GUI } from 'lil-gui';
+import { countries as data } from '../dataset';
+import { GraphRenderer } from '../utils/renderer';
+
+export function render(gui?: GUI) {
+  const renderer = new GraphRenderer();
+
+  const { width, height } = renderer.getCanvasSize();
+
+  const random = new MDSLayout({
+    center: [width / 2, height / 2],
+  });
+
+  const relayout = async (options = {}) => {
+    await random.execute(data, options);
+    renderer.render(random, {
+      nodeStyle: { stroke: '#F875AA', lineWidth: 1 },
+    });
+  };
+
+  relayout();
+
+  return renderer.getCanvas();
+}
